db63a1e898a0da0b623d081bad4453842d377e37
Slither8 <alexander.eckmaier@gmail.com>
2014-12-13 15:11:07 +0100

    Overview und Details Tab geadded. Price TextView ist clickable --> onClick="go_to_details" --> es soll für jede Stock-Zeile ein neuer/eigener Details Tab erstellt werden (jedoch nur 1x!), das ist noch nicht umgesetzt, möchte ich aber heute eventuell noch machen

---
+++ AndroidClient/StockServiceClient/src/main/java/client/android/ListAdapter.java
@@ -0,0 +1,44 @@
+package main.java.client.android;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import com.example.stockserviceclient.R;
+
+public class ListAdapter extends ArrayAdapter<Stock> {
+	
+	LayoutInflater inflater;
+	int resourceId;
+	Context ctx;
+	public ListAdapter(Context context, int resourceId,
+			ArrayList<Stock> person) {
+		super(context, resourceId, person);
+		inflater = LayoutInflater.from(context);
+		this.resourceId = resourceId;	
+		this.ctx = context;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		
+		convertView = ( RelativeLayout ) inflater.inflate( resourceId, null );
+        
+        Stock stock = getItem( position );
+        
+        TextView id = (TextView) convertView.findViewById(R.id.tv_stockId);
+        id.setText(stock.getId());
+        TextView price = (TextView) convertView.findViewById(R.id.tv_stockPrice);
+        price.setText(stock.getPrice() + "");  
+
+        return convertView;
+	}
+	
+	
+
+}
